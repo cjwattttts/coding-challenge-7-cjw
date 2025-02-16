3955f20cd59dc56e577f09d67884edfbfe5a9b2b
cjwattttts <cameron.jwatts@outlook.com>
2025-02-16 10:48:54 -0500

    Task 5 - Business Loan Interest Calculation

--- cc_7.js
+++ cc_7.js
@@ -51,3 +51,11 @@ function calculateShippingCost(weight, location, expedited = false) { // calcula
 
 console.log(calculateShippingCost(10, "USA", true)); // Expected output: "Shipping Cost: $20.00"
 console.log(calculateShippingCost(5, "Canada", false)); // Expected output: "Shipping Cost: $13.50"
+
+// TASK 5
+function calculateLoanInterest(principal, rate, years) { // calculates total interest
+    let interest = principal * rate * years;
+return `Interest Total: $${interest.toFixed(2)}`}
+
+console.log(calculateLoanInterest(1000, 0.05, 3)); // Expected output: "Total Interest: $150.00"
+console.log(calculateLoanInterest(5000, 0.07, 5)); // Expected output: "Total Interest: $1750.00"
